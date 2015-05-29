163e15ab56a77b6ef0e377a239ec558014d891ab
Brian Woodward <brian.woodward@gmail.com>
2015-05-28 22:32:57 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * level-id-gen <https://github.com/doowb/level-id-gen>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
