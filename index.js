db55176e7abf4caaa634609fdbb31499aa18f39c
Brian Woodward <brian.woodward@gmail.com>
2015-05-29 00:01:21 -0400

    id-gen deps

--- index.js
+++ index.js
@@ -7,6 +7,42 @@
 
 'use strict';
 
-module.exports = function () {
-  // do stuff
+module.exports = function (db) {
+  if (typeof db === 'undefined') {
+    throw new Error('Expected db to be provided.');
+  }
+  var digits = require('digits');
+  var extend = require('extend-shallow');
+  var plus = require('levelplus');
+  db = plus(db);
+
+  return function generator (groupName, options, cb) {
+    if (typeof groupName === 'function') {
+      cb = groupName;
+      options = {};
+      groupName = null;
+    }
+
+    if (typeof options === 'function') {
+      cb = options;
+      options = {};
+    }
+
+    if (typeof cb !== 'function') {
+      throw new Error('generator expects a callback function');
+    }
+
+    groupName = groupName || 'default';
+    options = options || {};
+    var group = this.groups[groupName] || this.groups['default'];
+    var opts = extend({}, this.options, group.options, options);
+    var len = opts.digits || (opts.auto && String(opts.auto).length) || 3;
+    db.inc([groupName], 0, function (err, count) {
+      if (err) return cb(err);
+      group.counter = count;
+      var result = digits(''+group.counter, len);
+      result = (opts.prefix || '') + result;
+      return cb(null, result);
+    });
+  };
 };
---
+++ test.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var path = require('path');
+var level = require('level');
+var async = require('async');
+var rimraf = require('rimraf');
+var mkdirp = require('mkdirp');
+var assert = require('assert');
+var bytewise = require('bytewise');
+var defaults = require('level-defaults');
+var sublevel = require('level-sublevel/bytewise');
+var digits = require('digits');
+
+var opts = {
+  keyEncoding: bytewise,
+  valueEncoding: 'json'
+};
+
+var dbPath = path.join(__dirname, '.data');
+var tables = [
+  'product',
+  'contact',
+  'account',
+  'user',
+  'item',
+  'invoice',
+  'order'
+];
+
+var db = null;
+var ids = null;
+
+describe('level-id-gen', function () {
+  beforeEach(function (done) {
+    rimraf(dbPath, function (err) {
+      if (err) return done(err);
+      mkdirp.sync(dbPath);
+      db = level(path.join(dbPath, 'db'));
+      db = defaults(db, opts);
+      db = sublevel(db);
+
+      var identities = db.sublevel('identities');
+      var generator = require('./')(identities);
+      var IdGen = require('id-gen');
+      ids = new IdGen(generator);
+      tables.forEach(function (table) {
+        ids.create(table, { digits: 10, prefix: table.toUpperCase() + '-' });
+      });
+      done();
+    });
+  });
+
+  afterEach(rimraf.bind(rimraf, dbPath));
+
+  it('should make ids', function (done) {
+    async.eachSeries(tables, function (table, next) {
+      var i = 0;
+      async.until(function () { return (i++) === 100;},
+        function (cb) {
+          var expected = table.toUpperCase() + '-' + digits(''+i, 10, '0');
+          ids.next(table, function (err, id) {
+            if (err) return cb(err);
+            assert(id, expected);
+            cb();
+          });
+        }, next);
+    }, done);
+  });
+});
+
+
+
+
+
+
+
+
+
+
